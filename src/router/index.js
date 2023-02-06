25794d58f50c6bc63d2f97b193fd04d573ead3bb
jinweios <jinwei.peng@outlook.com>
2023-02-06 14:34:12 +0800

    feat: 后端添加用户

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ const KoaRouter = require('koa-router')
 const loginCtrl = require('../controller/login/index')
 const uploadCtrl = require('../controller/upload/index')
 const uploadMultiCtrl = require('../controller/upload/upload-mul')
+const addUserCtrl = require('../controller/user/index')
 
 const router = new KoaRouter({
     prefix: '/api'
@@ -9,6 +10,7 @@ const router = new KoaRouter({
 
 // 写路由规则
 router.post('/login', loginCtrl)
+router.post('/user/add', addUserCtrl)
 
 // 上传文件
 router.post('/upload', uploadCtrl)
