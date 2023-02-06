000f7ddf2cc0d5d5e7045c0d9c99a4af8232aea4
jinweios <jinwei.peng@outlook.com>
2023-02-06 12:37:50 +0800

    feat: 后端代码'”

--- src/controller/login/index.js
+++ src/controller/login/index.js
@@ -1,14 +1,12 @@
 const { success, fail } = require('../../utils/response')
 const { createToken } = require('../../service/jwt')
+const { valid } = require('../../service/user')
 
-function login(ctx) {
-    const { username, password } = ctx.request.body
-    const user = {
-        name: '123456',
-        password: '123456'
-    }
-    if (username === user.name && password === user.password) {
-        const token = createToken({ username, password })
+async function login(ctx) {
+    const { email, password } = ctx.request.body
+    const isValid = await valid({ email, password });
+    if (isValid) {
+        const token = createToken({ email, password })
         ctx.body = success({ token })
     } else {
         ctx.body = fail('用户名或者密码错误')
